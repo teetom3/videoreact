2819cd13f392aed0ed26697a5acfbf1084514b25
thomas <thomas.zeryouh@goons.fr>
2025-06-27 13:20:59 +0200

    correctio act fin

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,6 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import { removeComment } from "./../redux/commentSlice";
-import { Card, Col, Row, Button } from "react-bootstrap";
+import {
+  Card,
+  Col,
+  Row,
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Alert,
+} from "react-bootstrap";
 
 function Comments() {
   const comments = useSelector((state) => state.comments.comments);
@@ -14,40 +22,31 @@ function Comments() {
     <Row className="mt-4">
       <Col md={6} className="offset-md-3">
         {comments.length === 0 ? (
-          <div className="alert alert-info mb-0">
-            Aucun commentaire pour le moment.
-          </div>
+          <Alert variant={"info"}>Aucun commentaire pour le moment.</Alert>
         ) : (
-          <div>
+          <ListGroup>
             {comments.map((comment, index) => (
-              <div
-                key={comment.id}
-                className={`py-3 ${
-                  index !== comments.length - 1 ? "border-bottom" : ""
-                }`}
-              >
-                <Card className="p-3">
-                  <div className="d-flex justify-content-between align-items-start mb-2">
-                    <div>
-                      <div className="d-flex align-items-center mb-1">
-                        <strong>Note {comment.note}/5</strong>
-                      </div>
+              <ListGroupItem className="p-3">
+                <div className="d-flex justify-content-between align-items-start mb-2">
+                  <div>
+                    <div className="d-flex align-items-center mb-1">
+                      <strong>Note {comment.note}/5</strong>
                     </div>
                   </div>
-                  <div className="d-flex justify-content-between align-items-start mb-2">
-                    <p className="mb-0">{comment.comment}</p>
-                    <Button
-                      variant="danger"
-                      size="sm"
-                      onClick={() => handleDeleteComment(comment.id)}
-                    >
-                      Supprimer
-                    </Button>
-                  </div>
-                </Card>
-              </div>
+                </div>
+                <div className="d-flex justify-content-between align-items-start mb-2">
+                  <p className="mb-0">{comment.comment}</p>
+                  <Button
+                    variant="danger"
+                    size="sm"
+                    onClick={() => handleDeleteComment(comment.id)}
+                  >
+                    Supprimer
+                  </Button>
+                </div>
+              </ListGroupItem>
             ))}
-          </div>
+          </ListGroup>
         )}
       </Col>
     </Row>
